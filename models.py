375a009d932e4f48a9db8862b04aafffab7aefcf
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-05-14 11:25:47 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,8 +1,155 @@
-from tensorflow.keras.layers import Conv2D, Input, Reshape, RepeatVector, concatenate, UpSampling2D, Flatten, Conv2DTranspose
-from keras.models import Model
+from tensorflow.keras.layers import Conv2D, Input, Reshape, RepeatVector, concatenate,\
+    UpSampling2D, Flatten, Conv2DTranspose, Layer
+from tensorflow.keras.models import Model
 import tensorflow as tf
 from tensorflow.keras import backend as Kbackend
-from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
+import util
+import numpy as np
+from skimage import color
+import os
+os.environ["PATH"] += os.pathsep + r'C:\Program Files (x86)\Graphviz2.38\bin/'
+
+
+class NetworkModelManager:
+    def __init__(self, **kwargs):
+        self.name = kwargs.get("name", "network_model")
+        os.makedirs("training_data/"+self.name, exist_ok=True)
+        self.checkpoint_path = "training_data/"+self.name+"/cp-{epoch:04d}.ckpt"
+        self.history_path = f"training_data/{self.name}/cp-history.json"
+        self.checkpoint_dir = os.path.dirname(self.checkpoint_path)
+        self.history = dict()
+        self.model = None
+        self.current_epoch = 0
+
+    def load_weights(self):
+        latest_cp: str = tf.train.latest_checkpoint(self.checkpoint_dir)
+        self.current_epoch = int(latest_cp[latest_cp.find("cp-")+3:latest_cp.find(".ckpt")])
+        self.model.load_weights(latest_cp)
+
+    def save_weights(self, epoch=0):
+        self.model.save_weights(self.checkpoint_path.format(epoch=epoch))
+
+    def load_history(self):
+        import json
+        if os.path.exists(self.history_path):
+            self.history = json.load(open(self.history_path, 'r'))
+
+    def save_history(self):
+        import json
+        json.dump(self.history, open(self.history_path, 'w'))
+
+    def update_curr_epoch(self):
+        latest_cp: str = tf.train.latest_checkpoint(self.checkpoint_dir)
+        self.current_epoch = int(latest_cp[latest_cp.find("cp-") + 3:latest_cp.find(".ckpt")])
+
+    def update_history(self, other: dict):
+        temp = {**self.history, **other}
+        for key, value in temp.items():
+            if key in self.history and key in other:
+                if isinstance(value, list):
+                    temp[key] = self.history[key] + value
+                elif isinstance(value, np.ndarray):
+                    temp[key] = list(self.history[key]).extend(list(value))
+                else:
+                    temp[key] = [self.history[key], value]
+        self.history = temp
+        self.save_history()
+        self.update_curr_epoch()
+
+    def load(self):
+        self.load_weights()
+        self.load_history()
+
+    def save(self, epoch=0):
+        self.save_weights(epoch)
+        self.save_history()
+
+
+class CatColorizer(NetworkModelManager):
+    def __init__(self, depth_after_fusion=256, img_size=128, learning_rate=1e-3, **kwargs):
+        super(CatColorizer, self).__init__(**kwargs)
+        self.depth_after_fusion = depth_after_fusion
+        self.img_size = img_size
+        self.lr = learning_rate
+
+        self.inception_resnet_v2_model = tf.keras.applications.InceptionResNetV2(
+            input_shape=(img_size, img_size, 3),
+            include_top=False,
+            weights='imagenet'
+        )
+        self.inception_resnet_v2_model.trainable = False
+
+    def build(self):
+        # encoder
+        encoder_input = Input(shape=(self.img_size, self.img_size, 1), name="encoder_input")
+        x = Conv2D(64, (3, 3), activation='relu', padding='same', strides=2)(encoder_input)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same', strides=2)(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same')(x)
+        x = Conv2D(256, (3, 3), activation='relu', padding='same', strides=2)(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+        x = Conv2D(512, (3, 3), activation='relu', padding='same')(x)
+        encoder_output = Conv2D(256, (3, 3), activation='relu', padding='same', name="encoder_output")(x)
+
+        batch, height, width, channels = Kbackend.int_shape(encoder_output)
+
+        # Fusion Layer
+        repeat_input = Kbackend.concatenate([encoder_input, encoder_input, encoder_input], axis=-1)
+        embs = self.inception_resnet_v2_model(repeat_input)
+        embs = tf.keras.layers.GlobalAveragePooling2D()(embs)
+
+        embs = RepeatVector(height * width)(embs)
+        embs = Reshape((height, width, embs.shape[-1]))(embs)
+        embs = concatenate([encoder_output, embs], axis=-1)
+
+        # decoder
+        decoder_input = Conv2D(self.depth_after_fusion, (1, 1), activation='relu', name="decoder_input")(embs)
+        x = Conv2D(128, (3, 3), activation='relu', padding='same')(decoder_input)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+        x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+        x = UpSampling2D((2, 2))(x)
+        x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+        x = Conv2D(2, (3, 3), activation='tanh', padding='same')(x)
+        decoder_output = UpSampling2D((2, 2), name="decoder_output")(x)
+        self.model = Model(encoder_input, decoder_output, name=self.name)
+
+        if len(os.listdir(self.checkpoint_dir)) == 0:
+            self.save_weights(epoch=0)
+        return self.model
+
+    def build_and_compile(self):
+        self.model = self.build()
+        self.model = self.compile()
+        return self.model
+
+    def compile(self):
+        assert self.model is not None
+        self.model.compile(
+            optimizer=Adam(self.lr),
+            loss='mae',
+            metrics=[
+                'accuracy',
+                'mse',
+                'mae',
+                'mape',
+                 # y_true_max,
+                 # y_true_min,
+                 # y_pred_max,
+                 # y_pred_min,
+            ]
+        )
+        return self.model
+
+    def predict_rgb(self, l_batch):
+        ab_batch = self.model.predict(l_batch)
+        lab_batch = np.zeros((l_batch.shape[0], l_batch.shape[1], l_batch.shape[2], 3))
+        lab_batch[:, :, :, 0] = util.unnormalize_l_channel(tf.squeeze(l_batch))
+        lab_batch[:, :, :, [1, 2]] = util.unnormalize_ab_channels(ab_batch)
+        rgb_batch = color.lab2rgb(lab_batch)
+
+        return rgb_batch
 
 
 # shows the minimum value of the AB channels
@@ -25,68 +172,18 @@ def y_pred_max(yt, yp):
     return Kbackend.max(yp)
 
 
-def generate_model(lr=1e-3, img_size=128):
-    '''
-    Creates a Colorizer model. Note the difference from the report
-    - https://github.com/baldassarreFe/deep-koalarization/blob/master/report.pdf
-    I use a long skip connection network to speed up convergence and
-    boost the output quality.
-    '''
-
-    # encoder model
-    encoder_ip = Input(shape=(img_size, img_size, 1))
-
-    encoder1 = Conv2D(64, (3, 3), padding='same', activation='relu', strides=(2, 2))(encoder_ip)
-
-    encoder = Conv2D(128, (3, 3), padding='same', activation='relu')(encoder1)
-
-    encoder2 = Conv2D(128, (3, 3), padding='same', activation='relu', strides=(2, 2))(encoder)
-
-    encoder = Conv2D(256, (3, 3), padding='same', activation='relu')(encoder2)
-    encoder = Conv2D(256, (3, 3), padding='same', activation='relu', strides=(2, 2))(encoder)
-    encoder = Conv2D(512, (3, 3), padding='same', activation='relu')(encoder)
-    encoder = Conv2D(512, (3, 3), padding='same', activation='relu')(encoder)
-    encoder = Conv2D(256, (3, 3), padding='same', activation='relu')(encoder)
-
-    # input fusion
-    # Decide the image shape at runtime to allow prediction on
-    # any size image, even if training is on 128x128
-    batch, height, width, channels = Kbackend.int_shape(encoder)
-
-    features_average_ip = tf.keras.layers.GlobalAveragePooling2D()
-
-    resnet_features = tf.keras.layers.Dense(1000)(features_average_ip)
-    fusion = RepeatVector(height * width)(resnet_features)
-    fusion = Reshape((height, width, 1000))(fusion)
-    fusion = concatenate([encoder, fusion], axis=-1)
-    fusion = Conv2D(256, (1, 1), padding='same', activation='relu')(fusion)
-
-    # decoder model
-    decoder = Conv2D(128, (3, 3), padding='same', activation='relu')(fusion)
-    decoder = UpSampling2D()(decoder)
-    decoder = concatenate([decoder, encoder2], axis=-1)
-    decoder = Conv2D(64, (3, 3), padding='same', activation='relu')(decoder)
-    decoder = Conv2D(64, (3, 3), padding='same', activation='relu')(decoder)
-    decoder = UpSampling2D()(decoder)
-    decoder = concatenate([decoder, encoder1], axis=-1)
-    decoder = Conv2D(32, (3, 3), padding='same', activation='relu')(decoder)
-    decoder = Conv2D(2, (3, 3), padding='same', activation='tanh')(decoder)
-    decoder = UpSampling2D((2, 2))(decoder)
-
-    model = Model([encoder_ip, features_average_ip], decoder, name='Colorizer')
-    model.compile(optimizer=Adam(lr), loss='mse', metrics=[y_true_max,
-                                                           y_true_min,
-                                                           y_pred_max,
-                                                           y_pred_min])
-
-    print("Colorization model built and compiled")
-    return model
-
-
 if __name__ == '__main__':
-    model = generate_model()
-    model.summary()
+    from tensorflow.keras.utils import plot_model
+    model_manager = CatColorizer(img_size=80, name="CatColorizer")
 
-    from keras.utils.vis_utils import plot_model
 
-    plot_model(model, to_file='skip_model.png', show_shapes=True)
+    new_history = {'loss': [1076.923828125], 'accuracy': [0.4869177043437958], 'mse': [0.010041841305792332], 'mae': [0.06346332281827927], 'mape': [1076.924072265625], 'val_loss': [330.6434020996094], 'val_accuracy': [0.7349596619606018], 'val_mse': [0.010745665989816189], 'val_mae': [0.06535135209560394], 'val_mape': [330.6432189941406]}
+    model_manager.load_history()
+    print(model_manager.history)
+    model_manager.update_history(new_history)
+    print(model_manager.history)
+
+
+    # model.summary()
+    # plot_model(model, to_file='Figures/CatColorizer.png', show_shapes=True)
+
