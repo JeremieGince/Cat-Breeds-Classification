f987fff9fecdc6128675e5c26f9a8189399df222
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-05-21 16:10:52 -0400

    Update Dataset.py

--- Dataset.py
+++ Dataset.py
@@ -41,6 +41,32 @@ class ColorizerDataGenerator:
         return next(self())
 
 
+class CatBreedsClassifierDataGenerator:
+    def __init__(self, image_data_generator, **kwargs):
+        self.image_data_generator = image_data_generator
+        self.virtual_length = kwargs.get("virtual_length", None)
+
+    @property
+    def labels(self):
+        return self.image_data_generator.labels
+
+    def __len__(self):
+        return len(self.image_data_generator) if self.virtual_length is None else self.virtual_length
+
+    def __call__(self):
+        while True:
+            yield self.image_data_generator.next()
+
+    def __next__(self):
+        return next(self())
+
+    def __iter__(self):
+        return self()
+
+    def next(self):
+        return next(self())
+
+
 class CatColorizerDataset:
     _URL = r"https://www.kaggle.com/ma7555/cat-breeds-dataset/download/"
     _DIR = r'C:\Users\gince\Documents\GitHub\Cat-Breeds-Classification\Data\cat-breeds-dataset/'
@@ -53,7 +79,7 @@ class CatColorizerDataset:
         self.IMG_SIZE = kwargs.get("img_size", CatColorizerDataset.IMG_SIZE)
         self.BATCH_SIZE = kwargs.get("batch_size", CatColorizerDataset.BATCH_SIZE)
 
-        # self.path_to_zip = tf.keras.utils.get_file('cat-breeds-dataset.zip', origin=self._URL, extract=True)
+        # self.path_to_zip = tf.keras.utils.get_file('cat-breeds-col_dataset.zip', origin=self._URL, extract=True)
         # print(f"Data saved to: {self.path_to_zip}")
         # self.PATH = os.path.join(os.path.dirname(self.path_to_zip), '/cat-breeds-dataset/images')
         self.PATH = os.path.join(os.path.dirname(self._DIR), 'images')
@@ -133,7 +159,100 @@ class CatColorizerDataset:
         plt.show()
 
 
+class CatBreedsClassifierDataset:
+    _URL = r"https://www.kaggle.com/ma7555/cat-breeds-dataset/download/"
+    _DIR = r'C:\Users\gince\Documents\GitHub\Cat-Breeds-Classification\Data\cat-breeds-dataset/'
+
+    BATCH_SIZE = 256
+    IMG_SIZE = 80  # must be more than 80, 80 is recommended
+    VAL_SPLIT = 0.9
+
+    def __init__(self, **kwargs):
+        self.IMG_SIZE = kwargs.get("img_size", CatColorizerDataset.IMG_SIZE)
+        self.BATCH_SIZE = kwargs.get("batch_size", CatColorizerDataset.BATCH_SIZE)
+
+        # self.path_to_zip = tf.keras.utils.get_file('cat-breeds-col_dataset.zip', origin=self._URL, extract=True)
+        # print(f"Data saved to: {self.path_to_zip}")
+        # self.PATH = os.path.join(os.path.dirname(self.path_to_zip), '/cat-breeds-dataset/images')
+        self.PATH = os.path.join(os.path.dirname(self._DIR), 'images')
+
+        self.image_generator = ImageDataGenerator(
+            rescale=1. / 255,
+            rotation_range=45,
+            width_shift_range=.15,
+            height_shift_range=.15,
+            horizontal_flip=True,
+            zoom_range=0.5,
+            validation_split=self.VAL_SPLIT,
+        )  # Generator for our training data
+
+        self.train_data_gen = CatBreedsClassifierDataGenerator(
+            self.image_generator.flow_from_directory(
+                batch_size=self.BATCH_SIZE,
+                directory=self.PATH,
+                shuffle=True,
+                seed=3,
+                target_size=(self.IMG_SIZE, self.IMG_SIZE),
+                class_mode="categorical",
+                subset="training"
+            )
+        )
+
+        self.val_data_gen = CatBreedsClassifierDataGenerator(
+            self.image_generator.flow_from_directory(
+                batch_size=self.BATCH_SIZE,
+                directory=self.PATH,
+                target_size=(self.IMG_SIZE, self.IMG_SIZE),
+                class_mode="categorical",
+                seed=3,
+                subset="validation"
+            ),
+            virtual_length=10_000 // self.BATCH_SIZE
+        )
+
+    @property
+    def nb_cls(self):
+        return len(self.labels)
+
+    @property
+    def train_length(self):
+        return len(self.train_data_gen)
+
+    @property
+    def val_length(self):
+        return len(self.val_data_gen)
+
+    @property
+    def labels(self):
+        return set(list(self.train_data_gen.labels) + list(self.val_data_gen.labels))
+
+    @property
+    def number_of_train_call(self):
+        return self.train_length // self.BATCH_SIZE
+
+    @property
+    def number_of_val_call(self):
+        return self.val_length // self.BATCH_SIZE
+
+    def plot_samples(self, nb_samples=5):
+        nb_samples = min(nb_samples, self.BATCH_SIZE)
+        rgb_batch, lbl_batch = next(self.train_data_gen)
+
+        fig, axes = plt.subplots(1, nb_samples, figsize=(10, 10))
+        for i in range(nb_samples):
+            axes[i].imshow(tf.squeeze(rgb_batch[i]))
+            axes[i].set_title(str(np.argmax(lbl_batch[i])))
+            axes[i].axis('off')
+
+        plt.tight_layout(pad=0.0)
+        plt.show()
+
+
 if __name__ == '__main__':
-    dataset = CatColorizerDataset(img_size=256, batch_size=100)
-    dataset.plot_samples(5)
+    col_dataset = CatColorizerDataset(img_size=256, batch_size=100)
+    col_dataset.plot_samples(5)
+
+    cls_dataset = CatBreedsClassifierDataset(img_size=256, batch_size=100)
+    cls_dataset.plot_samples(5)
+    print(f"cls_dataset.nb_cls: {cls_dataset.nb_cls}")
 
