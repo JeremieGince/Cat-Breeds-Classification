ed9123ea708a8db6e2c12011b26c256a5b9cb514
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-05-21 16:11:01 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,5 +1,5 @@
 from Dataset import CatColorizerDataset
-from models import NetworkModelManager, CatColorizer
+from models import NetworkModelManager, CatColorizer, NetworkManagerCallback
 import tensorflow as tf
 from tensorflow.keras.utils import plot_model
 import matplotlib.pyplot as plt
@@ -18,19 +18,21 @@ class Trainer:
         self.cp_callback = tf.keras.callbacks.ModelCheckpoint(
             filepath=self.modelManager.checkpoint_path,
             verbose=1,
-            save_weights_only=True,
+            save_weights_only=False,
             save_freq='epoch'
         )
+
+        self.network_callback = NetworkManagerCallback(self.modelManager)
 
     def train(self, epochs=1):
         self.modelManager.load()
         history = self.model.fit(
             self.dataset.train_data_gen(),
             steps_per_epoch=len(self.dataset.train_data_gen),
-            epochs=self.modelManager.current_epoch+epochs,
+            epochs=epochs,
             validation_data=self.dataset.val_data_gen(),
             validation_steps=len(self.dataset.val_data_gen),
-            callbacks=[self.cp_callback],
+            callbacks=[self.cp_callback, self.network_callback],
             verbose=1,
             initial_epoch=self.modelManager.current_epoch,
         )
@@ -39,17 +41,61 @@ class Trainer:
 
 if __name__ == '__main__':
     from util import plotHistory
-    BATCH_SIZE = 100
+    from models import CatBreedsClassifier
+    from Dataset import CatBreedsClassifierDataset
+    BATCH_SIZE = 64
     IMG_SIZE = 80
-    EPOCHS = 100
+    FEATURES_TRAINING_EPOCHS = 35
+    CLASSIFIER_EPOCHS = 10
     FUSION_DEPTH = 256
 
-    dataset = CatColorizerDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
+    col_dataset = CatColorizerDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
 
-    model_manager = CatColorizer(depth_after_fusion=FUSION_DEPTH, img_size=dataset.IMG_SIZE, name="CatColorizer")
-    model_manager.build_and_compile()
+    col_model_manager = CatColorizer(
+        depth_after_fusion=FUSION_DEPTH,
+        img_size=col_dataset.IMG_SIZE,
+        name="CatColorizer_GlorotNormal",
+    )
+    col_model_manager.build_and_compile()
 
-    trainer = Trainer(model_manager, dataset)
-    for _ in range(EPOCHS):
-        trainer.train()
-    plotHistory(model_manager.history, model_manager.current_epoch)
+    # Training of the features
+    col_trainer = Trainer(col_model_manager, col_dataset)
+    for e in range(FEATURES_TRAINING_EPOCHS):
+        col_trainer.train(e)
+    plotHistory(col_model_manager.history, col_model_manager.current_epoch)
+
+    cls_dataset = CatBreedsClassifierDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
+
+    # classification with pretrained features
+    cls_model_pretrained_manager = CatBreedsClassifier(
+        depth_after_fusion=FUSION_DEPTH,
+        img_size=IMG_SIZE,
+        output_size=cls_dataset.nb_cls,
+        cat_col_manager=col_model_manager,
+        name="CatBreedsClassifier_withPretrainedFeatures",
+        pretrained_head=True,
+    )
+    cls_model_pretrained_manager.build_and_compile()
+
+    cls_trainer = Trainer(cls_model_pretrained_manager, cls_dataset)
+    for e in range(CLASSIFIER_EPOCHS):
+        cls_trainer.train(e)
+    cls_model_pretrained_manager.load_history()
+    plotHistory(cls_model_pretrained_manager.history, cls_model_pretrained_manager.current_epoch)
+
+    # classification without pretrained features
+    cls_model_no_pretrained_manager = CatBreedsClassifier(
+        depth_after_fusion=FUSION_DEPTH,
+        img_size=IMG_SIZE,
+        output_size=cls_dataset.nb_cls,
+        cat_col_manager=col_model_manager,
+        name="CatBreedsClassifier_withoutPretrainedFeatures",
+        pretrained_head=False,
+    )
+    cls_model_no_pretrained_manager.build_and_compile()
+
+    cls_trainer = Trainer(cls_model_no_pretrained_manager, cls_dataset)
+    for e in range(CLASSIFIER_EPOCHS):
+        cls_trainer.train(e)
+    cls_model_no_pretrained_manager.load_history()
+    plotHistory(cls_model_no_pretrained_manager.history, cls_model_no_pretrained_manager.current_epoch)
