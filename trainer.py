73cfaac70206543f64a185f967f47d82075b56d6
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-05-26 22:10:22 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -1,4 +1,3 @@
-from Dataset import CatColorizerDataset
 from models import NetworkModelManager, CatColorizer, NetworkManagerCallback
 import tensorflow as tf
 from tensorflow.keras.utils import plot_model
@@ -7,95 +6,103 @@ import os
 
 
 class Trainer:
-    def __init__(self, model_manager: NetworkModelManager, dataset, **kwargs):
+    def __init__(self, model_manager: NetworkModelManager, dataset,
+                 network_callback_args=None,
+                 **kwargs):
+        if network_callback_args is None:
+            network_callback_args = {
+                "verbose": True,
+                "save_freq": 1
+            }
         self.BATCH_SIZE = kwargs.get("batch_size", 256)
         self.IMG_SIZE = kwargs.get("img_size", 80)
         self.FUSION_DEPTH = kwargs.get("fusion_depth", 128)
         self.modelManager = model_manager
         self.model = model_manager.model
         self.dataset = dataset
+        self.use_saving_callback = kwargs.get("use_saving_callback", True)
+        self.load_on_start = kwargs.get("load_on_start", True)
+        self.verbose = kwargs.get("verbose", 1)
 
-        self.cp_callback = tf.keras.callbacks.ModelCheckpoint(
-            filepath=self.modelManager.checkpoint_path,
-            verbose=1,
-            save_weights_only=False,
-            save_freq='epoch'
-        )
-
-        self.network_callback = NetworkManagerCallback(self.modelManager)
+        self.network_callback = NetworkManagerCallback(self.modelManager, **network_callback_args)
 
     def train(self, epochs=1):
-        self.modelManager.load()
+        if self.load_on_start:
+            self.modelManager.load()
         history = self.model.fit(
             self.dataset.train_data_gen(),
             steps_per_epoch=len(self.dataset.train_data_gen),
             epochs=epochs,
-            validation_data=self.dataset.val_data_gen(),
-            validation_steps=len(self.dataset.val_data_gen),
-            callbacks=[self.cp_callback, self.network_callback],
-            verbose=1,
+            validation_data=self.dataset.val_data_gen() if self.dataset.val_data_gen is not None else None,
+            validation_steps=len(self.dataset.val_data_gen) if self.dataset.val_data_gen is not None else None,
+            callbacks=[self.network_callback] if self.use_saving_callback else [],
+            verbose=self.verbose,
             initial_epoch=self.modelManager.current_epoch,
         )
-        self.modelManager.update_history(history.history)
+        self.modelManager.save_weights()
+        return history
 
 
 if __name__ == '__main__':
     from util import plotHistory
     from models import CatBreedsClassifier
-    from Dataset import CatBreedsClassifierDataset
-    BATCH_SIZE = 64
-    IMG_SIZE = 80
-    FEATURES_TRAINING_EPOCHS = 5
-    CLASSIFIER_EPOCHS = 5
-    FUSION_DEPTH = 256
+    from Dataset import CatBreedsClassifierDataset, CatColorizerOverfittingDataset, CatColorizerDataset
+    from hyperparameters import *
 
     col_dataset = CatColorizerDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
+    col_dataset.show_gamut_probabilities()
+    col_dataset.show_gamut_probabilities(rebin=True)
 
     col_model_manager = CatColorizer(
-        depth_after_fusion=FUSION_DEPTH,
+        col_dataset.gamut_instances,
+        fusion_depth=FUSION_DEPTH,
         img_size=col_dataset.IMG_SIZE,
-        name="CatColorizer",
+        name=f"CatColorizer_gamut-{col_dataset.GAMUT_SIZE}",
     )
     col_model_manager.build_and_compile()
 
-    # Training of the features
-    col_trainer = Trainer(col_model_manager, col_dataset)
-    for e in range(FEATURES_TRAINING_EPOCHS):
-        col_trainer.train(e)
-    plotHistory(col_model_manager.history, col_model_manager.current_epoch)
-
-    cls_dataset = CatBreedsClassifierDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
-
-    # classification with pretrained features
-    cls_model_pretrained_manager = CatBreedsClassifier(
-        depth_after_fusion=FUSION_DEPTH,
-        img_size=IMG_SIZE,
-        output_size=cls_dataset.nb_cls,
-        cat_col_manager=col_model_manager,
-        name="CatBreedsClassifier_withPretrainedFeatures",
-        pretrained_head=True,
+    # Training the features
+    col_trainer = Trainer(
+        col_model_manager,
+        col_dataset,
+        network_callback_args={
+            "verbose": True,
+            "save_freq": 1
+        }
     )
-    cls_model_pretrained_manager.build_and_compile()
+    col_trainer.train(FEATURES_TRAINING_EPOCHS)
+    plotHistory(col_model_manager.history)
 
-    cls_trainer = Trainer(cls_model_pretrained_manager, cls_dataset)
-    for e in range(CLASSIFIER_EPOCHS):
-        cls_trainer.train(e)
-    cls_model_pretrained_manager.load_history()
-    plotHistory(cls_model_pretrained_manager.history, cls_model_pretrained_manager.current_epoch)
+    # cls_dataset = CatBreedsClassifierDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
 
-    # classification without pretrained features
-    cls_model_no_pretrained_manager = CatBreedsClassifier(
-        depth_after_fusion=FUSION_DEPTH,
-        img_size=IMG_SIZE,
-        output_size=cls_dataset.nb_cls,
-        cat_col_manager=col_model_manager,
-        name="CatBreedsClassifier_withoutPretrainedFeatures",
-        pretrained_head=False,
-    )
-    cls_model_no_pretrained_manager.build_and_compile()
-
-    cls_trainer = Trainer(cls_model_no_pretrained_manager, cls_dataset)
-    for e in range(CLASSIFIER_EPOCHS):
-        cls_trainer.train(e)
-    cls_model_no_pretrained_manager.load_history()
-    plotHistory(cls_model_no_pretrained_manager.history, cls_model_no_pretrained_manager.current_epoch)
+    # classification with pre-trained features
+    # cls_model_pretrained_manager = CatBreedsClassifier(
+    #     depth_after_fusion=FUSION_DEPTH,
+    #     img_size=IMG_SIZE,
+    #     output_size=cls_dataset.nb_cls,
+    #     cat_col_manager=col_model_manager,
+    #     name="CatBreedsClassifier_withPretrainedFeatures",
+    #     pretrained_head=True,
+    # )
+    # cls_model_pretrained_manager.build_and_compile()
+    #
+    # cls_trainer = Trainer(cls_model_pretrained_manager, cls_dataset)
+    # cls_trainer.train(CLASSIFIER_EPOCHS)
+    # cls_model_pretrained_manager.load_history()
+    # plotHistory(cls_model_pretrained_manager.history)
+    #
+    # # classification without pre-trained features
+    # cls_model_no_pretrained_manager = CatBreedsClassifier(
+    #     depth_after_fusion=FUSION_DEPTH,
+    #     img_size=IMG_SIZE,
+    #     output_size=cls_dataset.nb_cls,
+    #     cat_col_manager=col_model_manager,
+    #     name="CatBreedsClassifier_withoutPretrainedFeatures",
+    #     pretrained_head=False,
+    # )
+    # cls_model_no_pretrained_manager.build_and_compile()
+    #
+    # cls_trainer = Trainer(cls_model_no_pretrained_manager, cls_dataset)
+    # cls_trainer.train(CLASSIFIER_EPOCHS)
+    # cls_model_no_pretrained_manager.load_history()
+    # plotHistory(cls_model_no_pretrained_manager.history)
