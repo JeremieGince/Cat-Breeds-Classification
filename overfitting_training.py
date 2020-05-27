0a2c6f54eb3e3f12657f2cbabb104a958ab9427a
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-05-26 22:10:15 -0400

    Create overfitting_training.py

---
+++ overfitting_training.py
@@ -0,0 +1,47 @@
+if __name__ == '__main__':
+    from trainer import Trainer
+    from util import plotHistory, plot_cat_colorization_prediction_samples
+    from models import CatBreedsClassifier, CatColorizer
+    from Dataset import CatBreedsClassifierDataset, CatColorizerOverfittingDataset
+    from hyperparameters import *
+
+    BATCH_SIZE = 1
+    IMG_SIZE = 80
+    FEATURES_TRAINING_EPOCHS = 1_000
+    BINS = 10
+    GAMUT_SIZE = 10
+
+    col_overfitting_dataset = CatColorizerOverfittingDataset(
+        gamut_size=GAMUT_SIZE,
+        bins=BINS,
+        img_size=IMG_SIZE,
+        batch_size=BATCH_SIZE
+    )
+    col_overfitting_dataset.show_gamut_probabilities(savefig=False)
+    col_overfitting_dataset.show_gamut_probabilities(rebin=True, savefig=False)
+
+    col_model_manager = CatColorizer(
+        col_overfitting_dataset.gamut_instances,
+        fusion_depth=FUSION_DEPTH,
+        img_size=col_overfitting_dataset.IMG_SIZE,
+        name=f"CatColorizer_overfitted_gamut-{col_overfitting_dataset.GAMUT_SIZE}",
+    )
+    col_model_manager.build_and_compile()
+    print(col_model_manager.summary)
+
+    # Training the features
+    col_model_manager.load()
+    col_trainer = Trainer(
+        col_model_manager,
+        col_overfitting_dataset,
+        use_saving_callback=True,
+        load_on_start=True,
+        network_callback_args={
+            "verbose": True,
+            "save_freq": 100
+        }
+    )
+    col_trainer.train(FEATURES_TRAINING_EPOCHS)
+    plotHistory(col_model_manager.history, savename="overfitted_training_curve")
+    plot_cat_colorization_prediction_samples(col_model_manager,
+                                             savename="overfitted_cat_colorization_prediction_samples")
