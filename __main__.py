428e244399776a52fb6eaa4af400246ebf79de75
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-05-26 22:08:33 -0400

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,7 +2,6 @@ import os
 
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import plot_model
 from skimage import color, transform
 import numpy as np
@@ -13,81 +12,75 @@ from models import CatColorizer, CatBreedsClassifier
 print(tf.__version__)
 
 
-def plot_cat_colorization_prediction_samples(model_manager, nb_samples=3):
-    image_generator = ImageDataGenerator(rescale=1. / 255)
-
-    data_gen = image_generator.flow_from_directory(
-        batch_size=nb_samples,
-        directory="Figures/Images_to_predict/",
-        shuffle=True,
-        target_size=(model_manager.img_size, model_manager.img_size),
-        class_mode=None,
-    )
-
-    rgb_batch = next(data_gen)
-
-    lab_batch = color.rgb2lab(rgb_batch).astype(np.float32)
-    l_batch = util.normalize_l_channel(lab_batch[:, :, :, 0][..., np.newaxis])
-    ab_batch = util.normalize_ab_channels(lab_batch[:, :, :, 1:])
-
-    rgb_batch_prediction = model_manager.predict_rgb(l_batch)
-
-    fig, axes = plt.subplots(nb_samples, 3, figsize=(10, 10))
-    for i in range(nb_samples):
-        axes[i][0].imshow(tf.squeeze(l_batch[i]), cmap='gray')
-        axes[i][1].imshow(rgb_batch_prediction[i])
-        axes[i][2].imshow(rgb_batch[i])
-
-        axes[i][0].axis('off')
-        axes[i][1].axis('off')
-        axes[i][2].axis('off')
-
-    axes[0][0].set_title("Input")
-    axes[0][1].set_title("Prediction")
-    axes[0][2].set_title("Ground truth")
-
-    plt.tight_layout(pad=0.0)
-    plt.show()
-
-
 def plot_history_comparison(pre_trained_history, no_pre_trained_history):
-    pass
+    fig, axes = plt.subplots(1, 2, figsize=(10, 10))
+
+    axes[0].plot(pre_trained_history["val_loss"], label="Pre-trained model")
+    axes[0].plot(no_pre_trained_history["val_loss"], label="No pre-trained model")
+    axes[0].set_title("Validation loss")
+    axes[0].set_xlabel("epochs")
+
+    axes[1].plot(pre_trained_history["val_accuracy"], label="Pre-trained model")
+    axes[1].plot(no_pre_trained_history["val_accuracy"], label="No pre-trained model")
+    axes[1].set_title("Validation accuracy")
+    axes[1].set_xlabel("epochs")
+
+    plt.legend(loc='upper right')
+    plt.show()
 
 
 if __name__ == '__main__':
     # -----------------------------------------------------------------------------------------------------------------
     # hyper-parameters
     # -----------------------------------------------------------------------------------------------------------------
-    BATCH_SIZE = 64
-    IMG_SIZE = 80
-    FEATURES_TRAINING_EPOCHS = 5
-    CLASSIFIER_EPOCHS = 5
-    FUSION_DEPTH = 256
+    from hyperparameters import SEED, \
+    BATCH_SIZE,\
+    IMG_SIZE, \
+    FEATURES_TRAINING_EPOCHS, \
+    CLASSIFIER_EPOCHS, \
+    FUSION_DEPTH, \
+    GAMUT_SIZE, \
+    BINS
+
+    # setting the seed
+    tf.random.set_seed(SEED)
 
     # -----------------------------------------------------------------------------------------------------------------
     # colorization
     # -----------------------------------------------------------------------------------------------------------------
 
-    col_dataset = CatColorizerDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
-    # col_dataset.plot_samples(3)
+    col_dataset = CatColorizerDataset(
+        gamut_size=GAMUT_SIZE,
+        bins=BINS,
+        img_size=IMG_SIZE,
+        batch_size=BATCH_SIZE
+    )
+    col_dataset.plot_samples(3)
+    col_dataset.show_gamut_probabilities()
+    col_dataset.show_gamut_probabilities(rebin=True)
+    print(f"dataset length: {len(col_dataset)}")
     print(f"dataset labels: {col_dataset.labels}")
 
-    col_model_manager = CatColorizer(depth_after_fusion=FUSION_DEPTH, img_size=col_dataset.IMG_SIZE,
-                                     name="CatColorizer")
+    col_model_manager = CatColorizer(
+        col_dataset.gamut_instances,
+        fusion_depth=FUSION_DEPTH,
+        img_size=col_dataset.IMG_SIZE,
+        name=f"CatColorizer_gamut-{col_dataset.GAMUT_SIZE}",
+    )
     col_model_manager.build_and_compile()
     col_model_manager.load()
 
     col_model_manager.model.summary()
-    plot_model(col_model_manager.model, to_file='Figures/CatColorizer.png', show_shapes=True)
-    util.plotHistory(col_model_manager.history, col_model_manager.current_epoch)
-    plot_cat_colorization_prediction_samples(col_model_manager)
-
     plot_model(col_model_manager.model, to_file=f"Figures/{col_model_manager.name}.png",
                show_layer_names=True, show_shapes=True)
+    util.plotHistory(col_model_manager.history)
+    util.plot_cat_colorization_prediction_samples(col_model_manager)
 
     # -----------------------------------------------------------------------------------------------------------------
     # Classification
     # -----------------------------------------------------------------------------------------------------------------
+
+    raise NotImplementedError("The classification part is not updated with the new colorizer")
 
     cls_dataset = CatBreedsClassifierDataset(img_size=IMG_SIZE, batch_size=BATCH_SIZE)
 
@@ -102,14 +95,13 @@ if __name__ == '__main__':
         name="CatBreedsClassifier_withPretrainedFeatures",
         pretrained_head=True,
     )
-
+    cls_model_pretrained_manager.build_and_compile()
     plot_model(cls_model_pretrained_manager.model,
                to_file=f"Figures/{cls_model_pretrained_manager.name}.png",
                show_layer_names=True, show_shapes=True)
 
-    cls_model_pretrained_manager.build_and_compile()
     cls_model_pretrained_manager.load_history()
-    util.plotHistory(cls_model_pretrained_manager.history, cls_model_pretrained_manager.current_epoch)
+    util.plotHistory(cls_model_pretrained_manager.history)
 
     # ---------------------------------------------------------
     # classification without pre-trained features
@@ -122,14 +114,13 @@ if __name__ == '__main__':
         name="CatBreedsClassifier_withoutPretrainedFeatures",
         pretrained_head=False,
     )
-
+    cls_model_no_pretrained_manager.build_and_compile()
     plot_model(cls_model_no_pretrained_manager.model,
                to_file=f"Figures/{cls_model_no_pretrained_manager.name}.png",
                show_layer_names=True, show_shapes=True)
 
-    cls_model_no_pretrained_manager.build_and_compile()
     cls_model_no_pretrained_manager.load_history()
-    util.plotHistory(cls_model_no_pretrained_manager.history, cls_model_no_pretrained_manager.current_epoch)
+    util.plotHistory(cls_model_no_pretrained_manager.history)
 
     # ---------------------------------------------------------
     # comparison
