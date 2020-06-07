ba03cb72f961a4c6fa5d36edbbe28d4091828a20
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-06-07 13:50:48 -0400

    Update __setup__.py

--- __setup__.py
+++ __setup__.py
@@ -3,4 +3,4 @@ from tensorflow.python.client import device_lib
 
 if __name__ == '__main__':
     print(f"Tensorflow version: {tensorflow.__version__}")
-    device_lib.list_local_devices()
+    print(*device_lib.list_local_devices(), sep='\n')
