4c96d9863211c6ad10516a61ba381eb6ec0c05dc
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-06-07 18:35:21 -0400

    Update __setup__.py

--- __setup__.py
+++ __setup__.py
@@ -4,3 +4,5 @@ from tensorflow.python.client import device_lib
 if __name__ == '__main__':
     print(f"Tensorflow version: {tensorflow.__version__}")
     print(*device_lib.list_local_devices(), sep='\n')
+    print(f"logical_devices (GPU): {tensorflow.config.list_logical_devices('GPU')}")
+    print(f"physical_devices (GPU): {tensorflow.config.list_physical_devices('GPU')}")
