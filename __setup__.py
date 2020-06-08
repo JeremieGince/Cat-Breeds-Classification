43bb093af796c2189786bbc104511487dc011ddd
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-06-08 14:34:41 -0400

    Update __setup__.py

--- __setup__.py
+++ __setup__.py
@@ -3,15 +3,17 @@ from tensorflow.python.client import device_lib
 import sys, os, platform
 
 if __name__ == '__main__':
-    print(f"Tensorflow version: {tensorflow.__version__}")
-    print(*device_lib.list_local_devices(), sep='\n')
-    print(f"logical_devices (GPU): {tensorflow.config.list_logical_devices('GPU')}")
-    print(f"physical_devices (GPU): {tensorflow.config.list_physical_devices('GPU')}")
+    print(f"Tensorflow version: {tensorflow.__version__} \n")
+    print(*device_lib.list_local_devices(), '\n', sep='\n')
+
+    if tensorflow.__version__ == "2.2.0":
+        print(f"logical_devices (GPU): {tensorflow.config.list_logical_devices('GPU')} \n")
+        print(f"physical_devices (GPU): {tensorflow.config.list_physical_devices('GPU')} \n")
 
     if platform.system() == "Linux":
-        print(os.system("nvidia-smi"))
+        print(os.system("nvidia-smi"), '\n')
     elif platform.system() == "Windows":
-        print(os.system(r"cd C:\Program Files\NVIDIA Corporation\NVSMI & nvidia-smi"))
+        print(os.system(r"cd C:\Program Files\NVIDIA Corporation\NVSMI & nvidia-smi"), '\n')
 
     inp = tensorflow.keras.layers.Input(10)
     test_model = tensorflow.keras.models.Model(inp, tensorflow.keras.layers.Dense(10)(inp))
