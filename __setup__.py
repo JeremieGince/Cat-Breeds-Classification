507066768ac0238745108ca57f5cec1717e7eef8
JeremieGince <50332514+JeremieGince@users.noreply.github.com>
2020-06-08 14:04:14 -0400

    Update __setup__.py

--- __setup__.py
+++ __setup__.py
@@ -1,8 +1,14 @@
 import tensorflow
 from tensorflow.python.client import device_lib
+import sys, os, platform
 
 if __name__ == '__main__':
     print(f"Tensorflow version: {tensorflow.__version__}")
     print(*device_lib.list_local_devices(), sep='\n')
     print(f"logical_devices (GPU): {tensorflow.config.list_logical_devices('GPU')}")
     print(f"physical_devices (GPU): {tensorflow.config.list_physical_devices('GPU')}")
+
+    if platform.system() == "Linux":
+        print(os.system("nvidia-smi"))
+    elif platform.system() == "Windows":
+        print(os.system(r"cd C:\Program Files\NVIDIA Corporation\NVSMI & nvidia-smi"))
